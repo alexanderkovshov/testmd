99aec35cbc87f578a3a159fd5d8835b2b40e02d2
alexanderkovshov <66995692+alexanderkovshov@users.noreply.github.com>
2021-10-21 16:41:21 +0300

    Create rby.rb

---
+++ rby.rb
@@ -0,0 +1,4 @@
+def weawe(asf, asf)
+    puts "demo"
+end
+<U+202E> chto to skritoe <U+202E><U+2066>puts "sasdasdsasdasdsasdasdsasdasdsasdasdsasdasdsasdasdsasdasdsasdasdsasdasdsasdasdsasdasdsasdasdsasdasdsasdasdsasdasdsasdasdsasdasdsasdasd"
